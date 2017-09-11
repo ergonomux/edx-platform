49332bef2b8385554ea4f8be97dee3dc9ba7c7b4
Sofiya Semenova <ssemenova@edx.org>
2017-09-11 13:27:02 -0400

    ED-1319 Cert not accessible by learner

--- lms/djangoapps/certificates/tasks.py
+++ lms/djangoapps/certificates/tasks.py
@@ -4,6 +4,7 @@ from logging import getLogger
 from celery_utils.logged_task import LoggedTask
 from celery_utils.persist_on_failure import PersistOnFailureTask
 from django.contrib.auth.models import User
+from lms.djangoapps.verify_student.models import SoftwareSecurePhotoVerification
 from opaque_keys.edx.keys import CourseKey
 
 from .api import generate_user_certificates
@@ -18,11 +19,32 @@ class _BaseCertificateTask(PersistOnFailureTask, LoggedTask):  # pylint: disable
     abstract = True
 
 
-@task(base=_BaseCertificateTask)
-def generate_certificate(**kwargs):
+@task(base=_BaseCertificateTask, bind=True, default_retry_delay=30, max_retries=2)
+def generate_certificate(self, **kwargs):
     """
     Generates a certificate for a single user.
+
+    kwargs:
+        - student: The student for whom to generate a certificate.
+        - course_key: The course key for the course that the student is
+            receiving a certificate in.
+        - expected_verification_status: If it is passed in, will be 'approved'.
+            Passed in from _listen_for_track_change, which runs when a learner's
+            verification status is changed to verified, thus kicking off a
+            certificate generation task. When the status has changed, we
+            double check that the actual verification status is as expected
+            before generating a certificate, in the off chance that the
+            database has not yet updated with the user's new verification
+            status.
     """
     student = User.objects.get(id=kwargs.pop('student'))
     course_key = CourseKey.from_string(kwargs.pop('course_key'))
-    generate_user_certificates(student=student, course_key=course_key, **kwargs)
+    expected_verification_status = kwargs.pop('expected_verification_status', None)
+    if not expected_verification_status:
+        generate_user_certificates(student=student, course_key=course_key, **kwargs)
+    else:
+        actual_verification_status, _ = SoftwareSecurePhotoVerification.user_status(student)
+        if expected_verification_status == actual_verification_status:
+            generate_user_certificates(student=student, course_key=course_key, **kwargs)
+        else:
+            self.retry(kwargs=kwargs)
--- lms/djangoapps/certificates/tests/test_tasks.py
+++ lms/djangoapps/certificates/tests/test_tasks.py
@@ -3,8 +3,11 @@ from unittest import TestCase
 import ddt
 from mock import patch
 from opaque_keys.edx.keys import CourseKey
+from nose.tools import assert_true
 
 from lms.djangoapps.certificates.tasks import generate_certificate
+from lms.djangoapps.verify_student.tests.factories import SoftwareSecurePhotoVerificationFactory
+from student.tests.factories import UserFactory
 
 
 @ddt.ddt
@@ -13,8 +16,13 @@ class GenerateUserCertificateTest(TestCase):
     @patch('lms.djangoapps.certificates.tasks.User.objects.get')
     def test_cert_task(self, user_get_mock, generate_user_certs_mock):
         course_key = 'course-v1:edX+CS101+2017_T2'
-
-        generate_certificate(student='student-id', course_key=course_key, otherarg='c', otherotherarg='d')
+        kwargs = {
+            'student': 'student-id',
+            'course_key': course_key,
+            'otherarg': 'c',
+            'otherotherarg': 'd'
+        }
+        generate_certificate.apply_async(kwargs=kwargs)
 
         expected_student = user_get_mock.return_value
         generate_user_certs_mock.assert_called_with(
@@ -32,4 +40,21 @@ class GenerateUserCertificateTest(TestCase):
 
         with patch('lms.djangoapps.certificates.tasks.User.objects.get'):
             with self.assertRaisesRegexp(KeyError, missing_arg):
-                generate_certificate(**kwargs)
+                generate_certificate.apply_async(kwargs=kwargs).get()
+
+    @patch('lms.djangoapps.certificates.tasks.generate_certificate.retry')
+    @patch('lms.djangoapps.certificates.tasks.generate_user_certificates')
+    def test_cert_task_retry(self, generate_user_certs_mock, mock_retry):
+        course_key = 'course-v1:edX+CS101+2017_T2'
+        student = UserFactory()
+
+        kwargs = {
+            'student': student.id,
+            'course_key': course_key,
+            'expected_verification_status': 'approved'
+        }
+        generate_certificate.apply_async(kwargs=kwargs)
+
+        SoftwareSecurePhotoVerificationFactory.create(user=student, status='approved')
+
+        self.assertTrue(mock_retry.called)
